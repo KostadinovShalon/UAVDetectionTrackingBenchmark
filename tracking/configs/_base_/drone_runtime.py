efb9ef01aa707f22719589b7ea290ba1e306fbf7
Matt <matthew.poyser@durham.ac.uk>
2021-06-23 16:36:42 +0100

    readability overhaul

---
+++ tracking/configs/_base_/drone_runtime.py
@@ -0,0 +1,14 @@
+_base_ = [
+    'default_runtime.py'
+]
+# learning policy
+lr_config = dict(
+    policy='step',
+    warmup='linear',
+    warmup_iters=100,
+    warmup_ratio=1.0 / 100,
+    step=[3])
+# runtime settings
+total_epochs = 4
+evaluation = dict(metric=['bbox', 'track'], interval=1)
+search_metrics = ['MOTA', 'IDF1', 'FN', 'FP', 'IDs', 'MT', 'ML']
