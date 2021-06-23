747f2e6e9df987423974c59640876f104ce71717
Matt <matthew.poyser@durham.ac.uk>
2021-06-23 16:37:52 +0100

    readability overhaul

---
+++ tracking/configs/_base_/models/detr_base.py
@@ -0,0 +1,55 @@
+model = dict(
+    detector=dict(
+    type='DETR',
+    pretrained='torchvision://resnet50',
+    backbone=dict(
+        type='ResNet',
+        depth=50,
+        num_stages=4,
+        out_indices=(3, ),
+        frozen_stages=1,
+        norm_cfg=dict(type='BN', requires_grad=False),
+        norm_eval=True,
+        style='pytorch'),
+    bbox_head=dict(
+        type='TransformerHead',
+        num_classes=80,
+        in_channels=2048,
+        num_fcs=2,
+        transformer=dict(
+            type='Transformer',
+            embed_dims=256,
+            num_heads=8,
+            num_encoder_layers=6,
+            num_decoder_layers=6,
+            feedforward_channels=2048,
+            dropout=0.1,
+            act_cfg=dict(type='ReLU', inplace=True),
+            norm_cfg=dict(type='LN'),
+            num_fcs=2,
+            pre_norm=False,
+            return_intermediate_dec=True),
+        positional_encoding=dict(
+            type='SinePositionalEncoding', num_feats=128, normalize=True),
+        loss_cls=dict(
+            type='CrossEntropyLoss',
+            bg_cls_weight=0.1,
+            use_sigmoid=False,
+            loss_weight=1.0,
+            class_weight=1.0),
+        loss_bbox=dict(type='L1Loss', loss_weight=5.0),
+        loss_iou=dict(type='GIoULoss', loss_weight=2.0)),
+    # training and testing settings
+    train_cfg=dict(
+        assigner=dict(
+            type='HungarianAssigner',
+            cls_cost=dict(type='ClassificationCost', weight=1.),
+            reg_cost=dict(type='BBoxL1Cost', weight=5.0),
+            iou_cost=dict(type='IoUCost', iou_mode='giou', weight=2.0))),
+    test_cfg=dict(max_per_img=100),)
+
+    # test_cfg=dict(
+    #     score_thr=0.05,
+    #     nms=dict(type='nms', iou_threshold=0.5),
+    #     max_per_img=100))
+)
---
+++ tracking/configs/_base_/models/tracktor_yolo_base.py
@@ -0,0 +1,46 @@
+_base_ = [
+    'yolo_base.py'
+]
+model = dict(
+    detector=dict(
+        bbox_head=dict(
+            num_classes=1)
+    ),
+    reid=dict(
+        type='BaseReID',
+        backbone=dict(
+            type='ResNet',
+            depth=50,
+            num_stages=4,
+            out_indices=(3, ),
+            style='pytorch'),
+        neck=dict(type='GlobalAveragePooling', kernel_size=(8, 4), stride=1),
+        head=dict(
+            type='LinearReIDHead',
+            num_fcs=1,
+            in_channels=2048,
+            fc_channels=1024,
+            out_channels=128,
+            norm_cfg=dict(type='BN1d'),
+            act_cfg=dict(type='ReLU'))),
+    motion=dict(
+        type='CameraMotionCompensation',
+        warp_mode='cv2.MOTION_EUCLIDEAN',
+        num_iters=100,
+        stop_eps=0.00001),
+    tracker=dict(
+        type='TracktorTracker',
+        obj_score_thr=0.5,
+        regression=dict(
+            obj_score_thr=0.5,
+            nms=dict(type='nms', iou_threshold=0.6),
+            match_iou_thr=0.3),
+        reid=dict(
+            num_samples=10,
+            img_scale=(256, 128),
+            img_norm_cfg=None,
+            match_score_thr=2.0,
+            match_iou_thr=0.2),
+        momentums=None,
+        num_frames_retain=10)
+)
