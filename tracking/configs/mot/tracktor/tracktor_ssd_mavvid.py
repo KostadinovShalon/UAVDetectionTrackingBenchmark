c00cdf12086692ec02e43e07a0750db49d8f7baf
MattPoyser <45001670+MattPoyser@users.noreply.github.com>
2021-06-23 16:47:58 +0100

    readability overhaul

---
+++ tracking/configs/mot/tracktor/tracktor_ssd_mavvid.py
@@ -0,0 +1,60 @@
+_base_ = [
+    '../../_base_/models/tracktor_ssd_base.py',
+    '../../_base_/datasets/mavvid_ncc.py',
+    '../../_base_/drone_runtime.py']
+model = dict(
+    type='Tracktor',
+    pretrains=dict(
+        detector=  # noqa: E251
+        # 'C:\\Users\\Matt\\Documents\\PhD\\drone\\mmtracking\\configs\\mot\\deepsort\\faster.pth', # noqa: E501
+        "/home2/lgfm95/drone/Strig-UAV-Project/MAV-VID/SSD512/latest.pth",
+        # "/hdd/PhD/drone/Strig-UAV-Project/anti-uav/SSD512/full/latest.pth",
+        reid=  # noqa: E251
+        'https://download.openmmlab.com/mmtracking/mot/reid/tracktor_reid_r50_iter25245-a452f51f.pth'  # noqa: E501
+        # "/hdd/PhD/tracking_wo_bnw/output/tracktor/reid/test/converted.pth"
+        # "/home2/lgfm95/drone/reid/antiuav.pth",
+
+    ),
+)
+img_norm_cfg = dict(mean=[123.675, 116.28, 103.53], std=[1, 1, 1], to_rgb=True)
+train_pipeline = [
+    dict(type='LoadImageFromFile', to_float32=True),
+    dict(type='LoadAnnotations', with_bbox=True),
+    dict(
+        type='PhotoMetricDistortion',
+        brightness_delta=32,
+        contrast_range=(0.5, 1.5),
+        saturation_range=(0.5, 1.5),
+        hue_delta=18),
+    dict(
+        type='Expand',
+        mean=img_norm_cfg['mean'],
+        to_rgb=img_norm_cfg['to_rgb'],
+        ratio_range=(1, 4)),
+    dict(
+        type='MinIoURandomCrop',
+        min_ious=(0.1, 0.3, 0.5, 0.7, 0.9),
+        min_crop_size=0.3),
+    dict(type='Resize', img_scale=(512, 512), keep_ratio=False),
+    dict(type='Normalize', **img_norm_cfg),
+    dict(type='RandomFlip', flip_ratio=0.5),
+    dict(type='DefaultFormatBundle'),
+    dict(type='Collect', keys=['img', 'gt_bboxes', 'gt_labels']),
+]
+test_pipeline = [
+    dict(type='LoadImageFromFile'),
+    dict(
+        type='MultiScaleFlipAug',
+        img_scale=(512, 512),
+        flip=False,
+        transforms=[
+            dict(type='Resize', keep_ratio=False),
+            dict(type='Normalize', **img_norm_cfg),
+            dict(type='ImageToTensor', keys=['img']),
+            dict(type='VideoCollect', keys=['img']),
+        ])
+]
+data = dict(
+    train=dict(pipeline=train_pipeline),
+    test=dict(pipeline=test_pipeline)
+)
